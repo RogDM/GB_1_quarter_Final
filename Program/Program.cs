20a92a601bf570e28d2be143bdd1fe11a84852f1
Dmitry Mikhailov <arlit112@gmail.com>
2022-09-14 19:53:38 +0300

    add Final

--- Program/Program.cs
+++ Program/Program.cs
@@ -3,6 +3,37 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
+string[] startMass = {"hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science"};
+string[] finalMass;
+
+int count = 0;
+int countMass = 0;
+
+for (int i = 0; i < startMass.Length; i++)
+{
+    count = startMass[i].Length;
+    if (count <= 3)
+    {
+        countMass++;
+    }
+}
+
+finalMass = new string[countMass];
+countMass = 0;
+
+for (int i = 0; i < startMass.Length; i++)
+{
+    count = startMass[i].Length;
+    if (count <= 3)
+    {
+        finalMass[countMass] = startMass[i];
+        countMass++;
+    }
+}
 
 
-Console.WriteLine("Hello, World!");
+for (int i = 0; i < finalMass.Length; i++)
+{
+    Console.Write($"{finalMass[i]} ");
+}
+
